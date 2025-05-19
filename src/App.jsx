8de0f25ee013056096aacff8db4b82825862b136
Marialafu <lafuentelhmaria@gmail.com>
2025-05-19 11:21:44 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Router from './lib/router/Router';
+import Menu from './components/menu/Menu';
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Router />
+      <Menu />
     </BrowserRouter>
   );
 };
